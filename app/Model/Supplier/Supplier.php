b8b1badffb572d2219fa3e8e2f417aa4ecf14fdc
ameyaaklekar <ameyaaklekar@gmail.com>
2020-08-09 17:34:36 +1200

    fixed the table name for supplier model

--- app/Model/Supplier/Supplier.php
+++ app/Model/Supplier/Supplier.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    protected $table = 'supplier';
+    protected $table = 'suppliers';
 
     public function company(){
         return $this->belongsTo('App\Model\Company\Company', 'company_id');
