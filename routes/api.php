1fb841da7f386ee29fd2d9663cd11e31f8fd9d2a
ameyaaklekar <ameyaaklekar@gmail.com>
2020-09-30 22:50:52 +1300

    Fixed the Routes to be more REST

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Company\CompanyController;
+use App\Http\Controllers\Employee\EmployeeController;
+use App\Http\Controllers\Permissions\PermissionsController;
+use App\Http\Controllers\Roles\RolesController;
+use App\Http\Controllers\Suppliers\SuppliersController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,47 +29,47 @@ Route::post('logout', [LogoutController::class, 'logout'])->middleware('auth:api
 Route::middleware('auth:api')->group(function() {
 
     Route::group(['prefix' => 'user'], function() {
-        Route::get('/', 'UserController@getUser');
+        Route::get('/', [UserController::class, 'getUser']);
     
-        Route::put('user/update', 'UserController@updateUser');
+        Route::put('/', [UserController::class, 'updateUser']);
     });
 
     Route::group(['prefix' => 'employee'], function() {     
-        Route::post('add', 'Employee\EmployeeController@addEmployee');
+        Route::post('/', [EmployeeController::class, 'addEmployee']);
     
-        Route::get('all', 'Employee\EmployeeController@getAllEmployees');
+        Route::get('/', [EmployeeController::class, 'getAllEmployees']);
     
-        Route::get('{employeeId}/edit', 'Employee\EmployeeController@getEmployee');
+        Route::get('{employeeId}', [EmployeeController::class, 'getEmployee']);
     
-        Route::put('update', 'Employee\EmployeeController@updateEmployee');
+        Route::put('/', [EmployeeController::class, 'updateEmployee']);
 
-        Route::patch('update/change-status', 'Employee\EmployeeController@changeStatus');
+        Route::patch('change-status', [EmployeeController::class, 'changeStatus']);
     });
 
     Route::group(['prefix' => 'roles'], function() {     
-        Route::get('/','Roles\RolesController@getRoles');
+        Route::get('/', [RolesController::class, 'getRoles']);
     
-        Route::get('permission/{role}','Roles\RolesController@getRolesPermission');
+        Route::get('permission/{role}', [RolesController::class, 'getRolesPermission']);
     });
 
     Route::group(['prefix' => 'permissions'], function() {     
-        Route::get('/','Permissions\PermissionsController@getPermissions');
+        Route::get('/', [PermissionsController::class, 'getPermissions']);
     });
    
     Route::group(['prefix' => 'suppliers'], function() {     
-        Route::post('add', 'Suppliers\SuppliersController@addSupplier');
+        Route::post('/', [SuppliersController::class, 'addSupplier']);
     
-        Route::get('all', 'Suppliers\SuppliersController@getAllSupplier');
+        Route::get('/', [SuppliersController::class, 'getAllSupplier']);
     
-        Route::put('update/change-status', 'Suppliers\SuppliersController@changeStatus');
+        Route::patch('change-status', [SuppliersController::class, 'changeStatus']);
     
-        Route::put('update', 'Suppliers\SuppliersController@updateSupplier');
+        Route::put('/', [SuppliersController::class, 'updateSupplier']);
     
-        Route::get('{supplierId}/edit', 'Suppliers\SuppliersController@getSupplier');
+        Route::get('{supplierId}', [SuppliersController::class, 'getSupplier']);
     });
 
     Route::group(['prefix' => 'company'], function() {
-        Route::get('employees', 'Company\CompanyController@getEmployees');
+        Route::get('employees', [CompanyController::class, 'getEmployees']);
     });
 
 });
